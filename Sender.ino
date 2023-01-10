fb7d2cce94cd90300c505f3e55a57028812269cd
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-01-11 02:02:57 +0600

    Update Sender.ino

--- Sender.ino
+++ Sender.ino
@@ -1,9 +1,9 @@
 //Sender Arduino Board Code (Master)
-char mystring[6] = "FARDIN";     //String data which is to be sent
+char mystring[15] = "LinuxHint.com  ";     //String data which is to be sent
 void setup() {                
  Serial.begin(9600);                 // Begin the Serial at 9600 Baud rate
 }
 void loop() {
- Serial.write(mystring,6);             //Write the serial data
+ Serial.write(mystring,15);             //Write the serial data
  delay(1000);
 }
