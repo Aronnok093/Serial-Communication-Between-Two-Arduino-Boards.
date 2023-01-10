1d2372f1186a3d52ae7a892ed3dfc7f3c7404c1a
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-01-10 23:57:30 +0600

    Add files via upload

---
+++ Rechiver.ino
@@ -0,0 +1,11 @@
+Code
+char mystring[20];               //Initialized variable to store receive
+void setup() {
+ Serial.begin(9600);             // Begin the Serial at 9600 Baud
+}
+void loop() {
+ Serial.readBytes(mystring,15);  //Read the serial data
+ Serial.println(mystring);       //Print data on Serial Monitor
+ Serial.println(" RECEIVER");
+ delay(1000);    
+}
